53af1b84484f61182f4384382b588a8e8f99fe8a
Martin Vidal Freyre <martinvidal.n7@gmail.com>
2023-04-21 16:57:09 -0300

    DC

--- Teoria/teoria/src/components/Prueba.jsx
+++ Teoria/teoria/src/components/Prueba.jsx
@@ -1,3 +1,20 @@
-export function Componente(){
-    return (<h1>Componente</h1>)
+import { useState } from "react"
+
+export function Prueba(){
+    const [likes, setLikes] = useState(0)
+
+    const darLike = () =>{
+        setLikes(likes + 1)
+    }
+
+    const quitarLike = () =>{
+        setLikes(likes - 1)
+    }
+
+    return (<div>
+        <h1>Likes {likes}</h1>
+        <button onClick={darLike}>Dar Like</button>
+        <button onClick={quitarLike}>Quitar Likes</button>
+    </div>)
+
 }
--- Teoria/teoria/src/index.jsx
+++ Teoria/teoria/src/index.jsx
@@ -4,21 +4,15 @@ import './index.css';
 //import {Coffe} from './components/Coffe';
 import {Usuarios} from './components/Usuarios';
 import { Card } from './components/Card'; 
+import { Prueba } from "./components/Prueba"; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Usuarios 
-    nombre="Mike" 
-    edad="24"
-    direccion={{
-      calle: "Gardaer", 
-      ciudad: "Caba",
-      pais: "Argentina"
-    }}
+    <Prueba 
+    
     />
 
-    <Card />
 
 
   </React.StrictMode>
